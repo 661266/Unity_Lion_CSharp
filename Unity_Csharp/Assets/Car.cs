797e9f746a37d89e7ca0c84c2093d6069c4715b7
661266 <a0989136675@gmail.com>
2020-04-08 15:30:36 +0800

    C#

--- Unity_Csharp/Assets/Car.cs
+++ Unity_Csharp/Assets/Car.cs
@@ -1,8 +1,10 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 
 // 註解:用來記錄只會在此腳本內出現
-// 3個/:是摘要 用來描述此內容，會在其他腳本出現-以便維護與擴充性
+// 3個/:是摘要 用來描述此內容，會在其他腳本出現-以便維護與擴充性(可讀性)
+//新增的C# 放在專案內為藍圖
+//放在場景物件上為實體物件
+
 public class Car : MonoBehaviour
 {
     //認識第一個成員:欄位(field)-儲存資料
@@ -16,13 +18,43 @@ public class Car : MonoBehaviour
     //用到小數點後面一訂要加 f
 
     //欄位的語法:
-    //資料類型 欄位名稱(指定值);
+    //修飾詞 資料類型 欄位名稱(指定值)
+    //公開 public :允許其他類別存取、顯示在屬性面板(Inspector)上
+    //私人 private:不允許；不顯示
+
     //預設值 int float 為0
     //string 為 ""
     //bool   為 false
 
-    int cc = 2000;
-    float weight = 100.9f;
-    string brand = "BMW";
-    bool brake = false;
+    //欄位的屬性 Attribute : Unity 提供用於輔助欄位的功能
+    //屬性語法:[屬性名稱(值)]
+    //標題[Header("字串")]  一個欄位只能用一個標題&提示
+    //提示[Tooltip("字串")] 一個欄位只能用一個標題&提示
+    //範圍[Range(最小值,最大值)]-僅限於數值類型資料 int,float
+    //也可以使用,來省略一個括號 ex: [Header("是否煞車"),Tooltip("用來儲存這台車子是否為煞車")]
+
+
+    /// <summary>
+    /// Car的cc
+    /// </summary>
+    [Header("CAR的CC數"),Range(500,5000)]
+    public int cc = 2000;
+    /// <summary>
+    /// Car的重量
+    /// </summary>
+    [Header("CAR重量")]
+    [Tooltip("這個車的重量")]
+    [Range(50,350)]
+    public float weight = 100.9f;
+    /// <summary>
+    /// Car的牌子
+    /// </summary>
+    [Header("品牌")]
+    [Tooltip("車的牌子")]
+    public string brand = "BMW";
+    /// <summary>
+    /// 是否煞車
+    /// </summary>
+    [Header("是否煞車"),Tooltip("用來儲存這台車子是否為煞車")]
+    public bool brake = false;
 }
--- Unity_Csharp/Assets/Class1.cs
+++ Unity_Csharp/Assets/Class1.cs
@@ -10,6 +10,11 @@ public class Class1 : MonoBehaviour
     //C#
     //1.大小不一樣(檔名也是)
     //2.括號包含: () {} [] "" '' <>
+    [Header("汽車1號")]
+    public Car Car1;
+
+    [Header("汽車2號")]
+    public Car Car2;
 
     //事件:在特定時間已指定次數執行的方法(藍色)
     //開始事件(Start):撥放遊戲時執行一次 - 初始設定 
@@ -18,5 +23,19 @@ public class Class1 : MonoBehaviour
         //輸出(print)("文字訊息")
         //C# 結尾為分號;
         print("嗨");
+
+
+
+        //取得 get
+        print("汽車1號的CC:" + Car1.cc);
+        print("汽車2號的牌子:" + Car2.brand);
+
+        //設定 set
+        Car1.cc = 9999;
+        Car1.weight = 999.9f;
+        Car1.brand = "TOYOTA";
+        Car1.brake = true;
+
+
     }
 }
