6cc9cb35f0d97cb6357e7affaa7025d4b11e4b07
661266 <a0989136675@gmail.com>
2020-05-05 10:53:05 +0800

    01

--- 05_Practice_UnityCSharp_OperatorMethod/Assets/bat.cs
+++ 05_Practice_UnityCSharp_OperatorMethod/Assets/bat.cs
@@ -11,10 +11,12 @@ public class bat : MonoBehaviour
     [Header("文字:訊息顯示")]
     public Text result;
     
+
     public void Attack(string batatt)
     {
         print(gameObject.name + "蝙蝠攻擊" + batatt + "攻擊10");
         print("史萊姆受傷HP剩餘:"+ (batATK- seraimuHP));
+
     }
 
 
--- 05_Practice_UnityCSharp_OperatorMethod/Assets/seraimu.cs
+++ 05_Practice_UnityCSharp_OperatorMethod/Assets/seraimu.cs
@@ -7,8 +7,14 @@ public class seraimu : MonoBehaviour
     public int seraimuhealing = (10);
     public int seraimuHP = (100);
     public int seraimuATK = (10);
+    public int batHP;
     [Header("文字:訊息顯示")]
     public Text result;
+    public void Attack(string batatt)
+    {
+        print(gameObject.name + "史萊姆攻擊" + batatt + "攻擊10");
+        print("蝙蝠受傷HP剩餘:" + (seraimuATK - batHP));
 
+    }
 
 }
